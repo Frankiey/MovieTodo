01d9284c035aa29981eb8b3a9ff91b440eba3600
frankieboyn <franknoorloos@hotmail.com>
2017-10-23 12:25:46 +0200

    Added comments and cleanup

--- MovieTodo/Models/Movie.cs
+++ MovieTodo/Models/Movie.cs
@@ -8,11 +8,14 @@ namespace MovieTodo.Models
 {
     public class Movie
     {
+        /// The Id of the movie
         public int ID { get; set; }
 
+        /// The title of the movie.
         [StringLength(60, MinimumLength = 3), Required]
         public string Title { get; set; }
 
+        /// String yes or no if the movie has been watched.
         public string Watched { get; set; }
     }
 }
--- MovieTodo/Models/OverviewModel.cs
+++ MovieTodo/Models/OverviewModel.cs
@@ -8,11 +8,14 @@ namespace MovieTodo.Models
 {
     public class OverviewModel
     {
+        /// The list of all movies.
         public List<Movie> movies;
 
+        /// The title of the movie.
         [StringLength(60, MinimumLength = 3), Required]
         public string Title { get; set; }
-
+        
+        /// String yes or no if the movie has been watched.
         public string Watched { get; set; }
     }
 }
